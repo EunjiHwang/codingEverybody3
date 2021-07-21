f7fe06b2a1346a62080f5ba5b40461df89c5ff9b
EunjiHwang <1101j@naver.com>
2021-07-21 14:47:26 +0900

    OOP - 7. 생성자와 this

---
+++ MyOOP5.java
@@ -0,0 +1,22 @@
+
+public class MyOOP5 {
+	public static void main(String[] args) {
+		Print3 p1 = new Print3("----");
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
+		
+		Print3 p2 = new Print3("****");
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
+		
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
+	}
+	
+}
